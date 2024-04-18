107ed3649fd1cd1e09cbd8ce1bd51a2a77c6c287
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-04-18 12:39:46 +0530

    New folder initiated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AirportComponent } from './pages/admin/airport/airport.component';
+import { AllFlightsComponent } from './pages/admin/all-flights/all-flights.component';
+import { BookingsComponent } from './pages/admin/bookings/bookings.component';
+import { CityComponent } from './pages/admin/city/city.component';
+import { LayoutComponent } from './pages/admin/layout/layout.component';
+import { LoginComponent } from './pages/admin/login/login.component';
+import { NewFlightComponent } from './pages/admin/new-flight/new-flight.component';
+import { BookFlightComponent } from './pages/website/book-flight/book-flight.component';
+import { SearchComponent } from './pages/website/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AirportComponent,
+    AllFlightsComponent,
+    BookingsComponent,
+    CityComponent,
+    LayoutComponent,
+    LoginComponent,
+    NewFlightComponent,
+    BookFlightComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
